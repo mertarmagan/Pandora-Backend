a28cb85cd02955931b7f19b37150249ee16399d3
Mert Armagan Sari <mertarmagan@windowslive.com>
2018-03-05 17:08:46 +0300

    Updated GameRoomHandler (added methods)

--- api.v0.2/game_rooms/GameRoomHandler.js
+++ api.v0.2/game_rooms/GameRoomHandler.js
@@ -11,7 +11,23 @@ module.exports['GameRoomHandler'] = function () {
     return {
         "roomList": Rooms.roomList,
         "size": Rooms.size,
-        "createGameRoom": function (roomObj) {
+        "createRoom": function (gameID, roomObj) {
+            let gameRoomID;
+            if(Rooms.size >= 1)
+              gameRoomID = Rooms.roomList[Rooms.size-1].gameRoomID + 1;
+            else
+              gameRoomID = 1;
+            roomObj['gameID']     = gameID;
+            // TODO Is there a better solution to id?
+            roomObj['gameRoomID'] = gameRoomID;
+            roomObj['users']      = [];
+            roomObj['active']     =  false;
+            Rooms.roomList.push(roomObj);
+            Rooms.size = Rooms.size + 1;
+            synchronize();
+            return { success: true, gameRoomID: gameRoomID };
+        },
+        "deleteRoom": function (roomObj) {
             roomObj['gameRoomID'] = Rooms.size; // TODO Is there a better solution to id?
             Rooms.roomList[Rooms.size] = roomObj;
             Rooms.size = Rooms.size + 1;
@@ -37,15 +53,28 @@ module.exports['GameRoomHandler'] = function () {
             synchronize();
         },
         "setGameRoomActive": function (gameRoomID, boolean) {
-            let done = false;
+            let success = false;
             Rooms.roomList.forEach(function (room) {
                 if(room['gameRoomID'] === gameRoomID){
                     room['active'] = boolean;
-                    done = true;
+                    success = true;
                 }
             });
             synchronize();
-            return done;
+            return success;
+        },
+        "wasPlaying": function (connID) {
+            let check = false;
+            for(var i=0; i<Rooms.size; i++){
+                for(var j=0; j<Rooms.roomList[i].length; j++){
+                    if(Rooms.roomList[i].users[j].connID === connID){
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            return { success: true, boolean: check };
         }
     }
 }();
