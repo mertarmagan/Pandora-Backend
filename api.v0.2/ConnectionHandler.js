4b136b0690a01596ab27356c6780ee8b55f06ce8
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-03-10 02:13:56 +0300

    Fixed Game Room Start. Some troubles with Game State getter..

--- api.v0.2/ConnectionHandler.js
+++ api.v0.2/ConnectionHandler.js
@@ -1,157 +1,230 @@
-module.exports ={
-    "isLeavingSafe": false,
-    "setLeavingSafe": function (boolean) {
-        this.isLeavingSafe = boolean;
-    },
-    "connectionLost": null,
-    "newConnection": null,
-    "ConnectionHandler": function () {
-        var webSocketServerPort = 1337;
-        var webSocketServer = require('websocket').server;
-        var GameStateHandler = require('./game_state/GameStateHandler').GameStateHandler;
-        var GameRoomHandler = require('./game_rooms/GameRoomHandler').GameRoomHandler;
-        var http = require('http');
-        var server = http.createServer(function (request,response) {
+module.exports = {
+  "isLeavingSafe": false,
+  "setLeavingSafe": function(boolean) {
+    this.isLeavingSafe = boolean;
+  },
+  "connectionLost": null,
+  "newConnection": null,
+  "ConnectionHandler": function() {
+    var webSocketServerPort = 1337;
+    var webSocketServer = require('websocket').server;
+    var GameStateHandler = require('./game_state/GameStateHandler').GameStateHandler;
+    var GameRoomHandler = require('./game_rooms/GameRoomHandler').GameRoomHandler;
+    var http = require('http');
+    var server = http.createServer(function(request, response) {
 
-        });
-        var LoginHandler = require('./LoginHandler').LoginHandler;
-        function htmlEntities(str) {
-            return String(str)
-                .replace(/&/g, '&amp;').replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-        }
+    });
+    var LoginHandler = require('./LoginHandler').LoginHandler;
 
-        server.listen(webSocketServerPort, function () {
-            console.log((new Date()) + " WSServer is listening on port "
-                + webSocketServerPort);
-        });
-        var wsServer = new webSocketServer({
-            httpServer: server
-        });
-
-        console.log(wsServer.httpServer);
-        let clients = [];
-
-        wsServer.on('request' , function (ws) {
-            console.log((new Date()) + ' Connection from origin '
-                + ws.origin + '.');
-            var connection = ws.accept();
-            connection.on('message' ,function (message) {
-                message = JSON.parse(message.utf8Data);
-                console.log(message);
-                if(message.type === "ADMIN_LOGIN"){
-                    // TODO: Try using ErrorHandler.assert(message.password);
-                    console.assert(message.password !== null);
-                    let key = LoginHandler.loginAdmin(message.password);
-                    if(key !== null)
-                        connection.send(JSON.stringify({type:"ADMIN_LOGIN" , key: key}));
-                    else
-                        connection.send(JSON.stringify({type:"ERROR", message: "Wrong Password"}));
-                }else if(message.type === "CREATE_GAME_ROOM") {
-                    console.assert(message.key !== null);
-                    console.assert(message.gameID !== null);
-                    if( LoginHandler.validateKey(message.key )){
-                        let roomObj = GameRoomHandler.createRoom(message.gameID);
-                        connection.send(JSON.stringify({type:"CREATE_GAME_ROOM" , gameRoom: roomObj}))
-                    }else {
-                        connection.send(JSON.stringify({type:"ERROR", message:"Game Room couldn't created"}))
-                    }
-                }else if(message.type === "DELETE_GAME_ROOM") {
-                    console.assert(message.key !== null);
-                    console.assert(message.gameRoomID !== null);
-                    if ( LoginHandler.validateKey(message.key)){
-                        let roomList = GameRoomHandler.deleteRoom(message.gameRoomID);
-                        connection.send(JSON.stringify({type:"DELETE_GAME_ROOM", roomList: roomList}))
-                    }else {
-                        connection.send(JSON.stringify({type:"ERROR", message:"Admin key not valid!"}))
-                    }
-                }else if(message.type === "DELETE_ALL_ROOMS") {
-                    console.assert(message.key !== null);
-                    if ( LoginHandler.validateKey(message.key)){
-                        let roomList = GameRoomHandler.deleteAllRooms();
-                        connection.send(JSON.stringify({type:"DELETE_ALL_ROOMS" , roomList: roomList}))
-                    }else {
-                        connection.send(JSON.stringify({type:"ERROR", message: "Couldn't delete all rooms"}))
-                    }
-                }else if(message.type === "GET_ACTIVE_GAME_ROOM") {
-                    let rooms = GameRoomHandler.getActiveRooms();
-                    connection.send(JSON.stringify({type: "GET_ACTIVE_GAME_ROOM", roomList: rooms}));
-                }else if(message.type === "GET_ALL_ROOMS") {
-                    let rooms = GameRoomHandler.getAllRooms();
-                    connection.send(JSON.stringify({type: "GET_ALL_ROOMS", roomList: rooms}))
-                }else if(message.type === "ENTER_GAME_ROOM"){
-                    console.assert(message.gameRoomID !== null);
-                    console.assert(message.username !== null);
-                    console.log("join game request: ", message);
-                    let newRoomState = GameRoomHandler.addUserToGameRoom(message.gameRoomID, message.username);
-                    clients.forEach(function (client) {
-                        if(client !== connection)
-                            client.send(JSON.stringify({type: "USER_JOINED" , room: newRoomState}))
-                    });
-                    connection.send(JSON.stringify({type: "ENTER_GAME_ROOM" , room: newRoomState}));
-                }else if(message.type === "EXIT_GAME_ROOM"){
-                    console.assert(message.gameRoomID !== null);
-                    console.assert(message.username !== null);
-                    let newRoomState = GameRoomHandler.deleteUserFromGameRoom(message.gameRoomID, message.username);
-                    clients.forEach(function (client) {
-                        client.send(JSON.stringify({type: "USER_EXIT" , gameRoomID: newRoomState.gameRoomID, room: newRoomState}))
-                    })
-                }else if(message.type === "SET_READY_TRUE"){
-                    console.assert(message.gameRoomID !== null);
-                    console.assert(message.username !== null);
-                    let newRoomState = GameRoomHandler.setUserReady(message.gameRoomID , message.username, true);
-                    clients.forEach(function (client) {
-                        client.send(JSON.stringify({type: "USER_READY", room: newRoomState}))
-                    }, this)
-                }else if(message.type === "SET_READY_FALSE"){
-                    console.assert(message.gameRoomID !== null);
-                    console.assert(message.username !== null);
-                    let newRoomState = GameRoomHandler.setUserReady(message.gameRoomID , message.username, false);
-                    clients.forEach(function (client) {
-                        client.send(JSON.stringify({type: "USER_READY", room: newRoomState}))
-                    }, this)
-                }else if(message.type === "GET_INITIAL_STATE"){
-                    console.assert(message.gameRoomID !== null);
-                    ws.send(JSON.stringify({type: "STATE_UPDATE", state: GameStateHandler.getGameState(message.gameRoomID)}))
-
-                }else if(message.type === "STATE_UPDATE") {
-                    console.assert(message.state !== null);
-                    console.assert(message.gameRoomID !== null);
-                    let newState = GameStateHandler.updateGameState(message.gameRoomID , message.state);
-                    clients.forEach(function (client) {
-                        if(connection !== client)
-                            client.send(JSON.stringify({type: "STATE_UPDATE", gameRoomID: newState.gameRoomID, state: newState}))
-                    })
-                }else if(message.type === "ENTER_GAME") {
-                    console.assert(message.gameRoomID !== null);
-                    var newState = GameStateHandler.getGameState(message.gameRoomID);
-                    if(newState)
-                        connection.send(JSON.stringify({type:"STATE_UPDATE", gameRoomID: newState.gameRoomID , state: newState}))
-                }
-                else {
-                    console.warn("Message did not recognized!")
-                }
-            });
-
-            let index = clients.push(connection) - 1;
-            console.log(clients.length);
-            if(this.newConnection)
-                this.newConnection(index);
-
-            connection.on('close' , function (connection) {
-                console.log((new Date()) + " Peer "
-                    + connection.remoteAddress + " disconnected.");
-                clients.splice(index, 1);
-                if(this.connectionLost)
-                    this.connectionLost(index);
-
-            }.bind(this));
-
-
-            connection.send(JSON.stringify({
-                type: 'GameStateSocketConnection',
-                data: 'Successful'
-            }))
-        });
+    function htmlEntities(str) {
+      return String(str)
+        .replace(/&/g, '&amp;').replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
     }
+
+    server.listen(webSocketServerPort, function() {
+      console.log((new Date()) + " WSServer is listening on port " +
+        webSocketServerPort);
+    });
+    var wsServer = new webSocketServer({
+      httpServer: server
+    });
+
+    console.log(wsServer.httpServer);
+    let clients = [];
+
+    wsServer.on('request', function(ws) {
+      console.log((new Date()) + ' Connection from origin ' +
+        ws.origin + '.');
+      var connection = ws.accept();
+      connection.on('message', function(message) {
+        message = JSON.parse(message.utf8Data);
+        console.log(message);
+        if (message.type === "ADMIN_LOGIN") {
+          // TODO: Try using ErrorHandler.assert(message.password);
+          console.assert(message.password !== null);
+          let key = LoginHandler.loginAdmin(message.password);
+          if (key !== null)
+            connection.send(JSON.stringify({
+              type: "ADMIN_LOGIN",
+              key: key
+            }));
+          else
+            connection.send(JSON.stringify({
+              type: "ERROR",
+              message: "Wrong Password"
+            }));
+        } else if (message.type === "CREATE_GAME_ROOM") {
+          console.assert(message.key !== null);
+          console.assert(message.gameID !== null);
+          if (LoginHandler.validateKey(message.key)) {
+            let roomObj = GameRoomHandler.createRoom(message.gameID);
+            connection.send(JSON.stringify({
+              type: "CREATE_GAME_ROOM",
+              gameRoom: roomObj
+            }))
+          } else {
+            connection.send(JSON.stringify({
+              type: "ERROR",
+              message: "Game Room couldn't created"
+            }))
+          }
+        } else if (message.type === "DELETE_GAME_ROOM") {
+          console.assert(message.key !== null);
+          console.assert(message.gameRoomID !== null);
+          if (LoginHandler.validateKey(message.key)) {
+            let roomList = GameRoomHandler.deleteRoom(message.gameRoomID);
+            connection.send(JSON.stringify({
+              type: "DELETE_GAME_ROOM",
+              roomList: roomList
+            }))
+          } else {
+            connection.send(JSON.stringify({
+              type: "ERROR",
+              message: "Admin key not valid!"
+            }))
+          }
+        } else if (message.type === "DELETE_ALL_ROOMS") {
+          console.assert(message.key !== null);
+          if (LoginHandler.validateKey(message.key)) {
+            let roomList = GameRoomHandler.deleteAllRooms();
+            connection.send(JSON.stringify({
+              type: "DELETE_ALL_ROOMS",
+              roomList: roomList
+            }))
+          } else {
+            connection.send(JSON.stringify({
+              type: "ERROR",
+              message: "Couldn't delete all rooms"
+            }))
+          }
+        } else if (message.type === "GET_ACTIVE_GAME_ROOM") {
+          let rooms = GameRoomHandler.getActiveRooms();
+          connection.send(JSON.stringify({
+            type: "GET_ACTIVE_GAME_ROOM",
+            roomList: rooms
+          }));
+        } else if (message.type === "GET_ALL_ROOMS") {
+          let rooms = GameRoomHandler.getAllRooms();
+          connection.send(JSON.stringify({
+            type: "GET_ALL_ROOMS",
+            roomList: rooms
+          }))
+        } else if (message.type === "ENTER_GAME_ROOM") {
+          console.assert(message.gameRoomID !== null);
+          console.assert(message.username !== null);
+          console.log("join game request: ", message);
+          let newRoomState = GameRoomHandler.addUserToGameRoom(message.gameRoomID, message.username);
+          clients.forEach(function(client) {
+            if (client !== connection)
+              client.send(JSON.stringify({
+                type: "USER_JOINED",
+                room: newRoomState
+              }))
+          });
+          connection.send(JSON.stringify({
+            type: "ENTER_GAME_ROOM",
+            room: newRoomState
+          }));
+        } else if (message.type === "START_GAME") {
+          console.log("Start game request!");
+          console.assert(message.gameRoomID !== null);
+          console.assert(message.username !== null);
+          let newRoomState = GameRoomHandler.startGame(message.gameRoomID);
+          clients.forEach(function(client) {
+            if (client !== connection)
+              client.send(JSON.stringify({
+                type: "START_GAME",
+                room: newRoomState
+              }))
+          });
+          connection.send(JSON.stringify({
+            type: "START_GAME",
+            room: newRoomState
+          }));
+        } else if (message.type === "EXIT_GAME_ROOM") {
+          console.assert(message.gameRoomID !== null);
+          console.assert(message.username !== null);
+          let newRoomState = GameRoomHandler.deleteUserFromGameRoom(message.gameRoomID, message.username);
+          clients.forEach(function(client) {
+            client.send(JSON.stringify({
+              type: "USER_EXIT",
+              gameRoomID: newRoomState.gameRoomID,
+              room: newRoomState
+            }))
+          })
+        } else if (message.type === "SET_READY_TRUE") {
+          console.assert(message.gameRoomID !== null);
+          console.assert(message.username !== null);
+          let newRoomState = GameRoomHandler.setUserReady(message.gameRoomID, message.username, true);
+          clients.forEach(function(client) {
+            client.send(JSON.stringify({
+              type: "USER_READY",
+              room: newRoomState
+            }))
+          }, this)
+        } else if (message.type === "SET_READY_FALSE") {
+          console.assert(message.gameRoomID !== null);
+          console.assert(message.username !== null);
+          let newRoomState = GameRoomHandler.setUserReady(message.gameRoomID, message.username, false);
+          clients.forEach(function(client) {
+            client.send(JSON.stringify({
+              type: "USER_READY",
+              room: newRoomState
+            }))
+          }, this)
+        } else if (message.type === "GET_INITIAL_STATE") {
+          console.assert(message.gameRoomID !== null);
+          ws.send(JSON.stringify({
+            type: "STATE_UPDATE",
+            state: GameStateHandler.getGameState(message.gameRoomID)
+          }))
+
+        } else if (message.type === "STATE_UPDATE") {
+          console.assert(message.state !== null);
+          console.assert(message.gameRoomID !== null);
+          let newState = GameStateHandler.updateGameState(message.gameRoomID, message.state);
+          clients.forEach(function(client) {
+            if (connection !== client)
+              client.send(JSON.stringify({
+                type: "STATE_UPDATE",
+                gameRoomID: newState.gameRoomID,
+                state: newState
+              }))
+          })
+        } else if (message.type === "ENTER_GAME") {
+          console.assert(message.gameRoomID !== null);
+          var newState = GameStateHandler.getGameState(message.gameRoomID);
+          if (newState)
+            connection.send(JSON.stringify({
+              type: "STATE_UPDATE",
+              gameRoomID: newState.gameRoomID,
+              state: newState
+            }))
+        } else {
+          console.warn("Message did not recognized!")
+        }
+      });
+
+      let index = clients.push(connection) - 1;
+      console.log(clients.length);
+      if (this.newConnection)
+        this.newConnection(index);
+
+      connection.on('close', function(connection) {
+        console.log((new Date()) + " Peer " +
+          connection.remoteAddress + " disconnected.");
+        clients.splice(index, 1);
+        if (this.connectionLost)
+          this.connectionLost(index);
+
+      }.bind(this));
+
+
+      connection.send(JSON.stringify({
+        type: 'GameStateSocketConnection',
+        data: 'Successful'
+      }))
+    });
+  }
 };
--- api.v0.2/game_rooms/GameRoomHandler.js
+++ api.v0.2/game_rooms/GameRoomHandler.js
@@ -1,144 +1,159 @@
 var fs = require('fs');
 const uuidv4 = require('uuid/v4');
 
-module.exports['GameRoomHandler'] = function () {
-    let roomData = fs.readFileSync('./game_rooms/rooms.json');
-    let Rooms = JSON.parse(roomData);
-    // let LoginHandler = require("./../LoginHandler").LoginHandler;
-    let roomList = Rooms.roomList;
-    let size     = Rooms.roomList.length;
+module.exports['GameRoomHandler'] = function() {
+  let roomData = fs.readFileSync('./game_rooms/rooms.json');
+  let Rooms = JSON.parse(roomData);
+  // let LoginHandler = require("./../LoginHandler").LoginHandler;
+  let roomList = Rooms.roomList;
+  let size = Rooms.roomList.length;
 
-    function synchronize(){
-        fs.writeFileSync('./game_rooms/rooms.json', JSON.stringify(Rooms, null, 2));
-    }
+  function synchronize() {
+    fs.writeFileSync('./game_rooms/rooms.json', JSON.stringify(Rooms, null, 2));
+  }
 
-    function generateGRID() {
-        return uuidv4();
-    }
+  function generateGRID() {
+    return uuidv4();
+  }
 
-    return {
-        "createRoom": function (gameID) {
-            let roomObj = {};
+  return {
+    "createRoom": function(gameID) {
+      let roomObj = {};
 
-            roomObj['gameID']     = gameID;
-            roomObj['gameRoomID'] = generateGRID();
-            roomObj['users']      = [];
-            roomObj['active']     = false;
-            roomObj['status']     = "init";
+      roomObj['gameID'] = gameID;
+      roomObj['gameRoomID'] = generateGRID();
+      roomObj['users'] = [];
+      roomObj['active'] = false;
+      roomObj['status'] = "init";
 
-            Rooms.roomList.push(roomObj);
-            Rooms.size += 1;
-            synchronize();
-            return roomObj;
-        },
-        "deleteRoom": function (gameRoomID) {
-            Rooms.roomList = Rooms.roomList.filter(function (room) {
-               return room.gameRoomID !== gameRoomID
-            });
-            Rooms.size -= 1;
-            synchronize();
-            return Rooms.roomList;
-        },
-        "deleteAllRooms": function () {
-            Rooms.roomList = [];
-            Rooms.size = 0;
-            synchronize();
-            return Rooms.roomList;
-        },
-        "duplicateUser": function (username, gameRoomID) {
-            for(let i=0; i<Rooms.size; i++){
-                if(roomList[i].gameRoomID === gameRoomID){
-                    for(let j=0; j<roomList[i].users.length; j++){
-                        if(roomList[i].users[j].username === username.toUpperCase()) {
-                            return true;
-                        }
-                    }
-                    break;
-                }
+      Rooms.roomList.push(roomObj);
+      Rooms.size += 1;
+      synchronize();
+      return roomObj;
+    },
+    "deleteRoom": function(gameRoomID) {
+      Rooms.roomList = Rooms.roomList.filter(function(room) {
+        return room.gameRoomID !== gameRoomID
+      });
+      Rooms.size -= 1;
+      synchronize();
+      return Rooms.roomList;
+    },
+    "deleteAllRooms": function() {
+      Rooms.roomList = [];
+      Rooms.size = 0;
+      synchronize();
+      return Rooms.roomList;
+    },
+    "duplicateUser": function(username, gameRoomID) {
+      for (let i = 0; i < Rooms.size; i++) {
+        if (roomList[i].gameRoomID === gameRoomID) {
+          for (let j = 0; j < roomList[i].users.length; j++) {
+            if (roomList[i].users[j].username === username.toUpperCase()) {
+              return true;
             }
-            return false;
-        },
-        "addUserToGameRoom": function (gameRoomID , username) {
-            let thisRoom = {};
-            let isExist = this.duplicateUser(username, gameRoomID);
-            Rooms.roomList.forEach(function (room) {
-                if(room['gameRoomID'] === gameRoomID){
-                    if(!isExist){
-                        console.log("user added");
-                        room.users.push({ username: username.toUpperCase(), ready: false });
-                        thisRoom = room;
-                    } else {
-                        thisRoom = null;
-                    }
-                }
-            }, thisRoom);
-            synchronize();
-            return thisRoom;
-        },
-        "deleteUserFromGameRoom": function(gameRoomID, username){
-            let thisRoom = {};
-            Rooms.roomList.forEach(function (room) {
-                if(room['gameRoomID'] === gameRoomID){
-                    room.users = room.users.filter(function (user) {
-                        return user.username !== username.toUpperCase()
-                    });
-                    thisRoom = room;
-                }
-            }, thisRoom);
-            synchronize();
-            return thisRoom;
-        },
-        "isGameRoomActive": function (gameRoomID) {
-            for(let i=0; i<size; i++){
-        		if(Rooms.roomList[i].gameRoomID === gameRoomID){
-        			return roomList[i].active;
-        		}
-        	}
-        },
-        "setGameRoomActive": function (gameRoomID, boolean) {
-            let success = false;
-            Rooms.roomList.forEach(function (room) {
-                if(room['gameRoomID'] === gameRoomID){
-                    room['active'] = boolean;
-                    success = true;
-                }
-            });
-            synchronize();
-            return success;
-        },
-        "setUserReady": function (gameRoomID, username, ready) {
-            let thisRoom = {};
-            for(let i=0; i<Rooms.roomList.length; i++){
-        		if(Rooms.roomList[i].gameRoomID === gameRoomID){
-        			for(let j=0; j<Rooms.roomList[i].users.length; j++){
-        				if(Rooms.roomList[i].users[j].username === username.toUpperCase()) {
-        					Rooms.roomList[i].users[j].ready = ready;
-        					break;
-        				}
-        			}
-        			thisRoom = Rooms.roomList[i];
-        			break;
-        		}
-        	}
-            synchronize();
-            return thisRoom;
-        },
-        "getActiveRooms": function () {
-            let activeGameRooms = [];
-            for(let i=0; i<Rooms.size; i++)
-        		if(Rooms.roomList[i].active === true)
-                   activeGameRooms.push(Rooms.roomList[i]);
-            return activeGameRooms;
-        },
-        "getAllRooms": function () {
-            return Rooms.roomList;
-        },
-        "wasPlaying": function (username) {
-            for(let i=0; i<Rooms.size; i++)
-                for(let j=0; j<Rooms.roomList[i].users.length; j++)
-                    if(Rooms.roomList[i].users[j].username === username.toUpperCase())
-                        return true;
-            return false;
+          }
+          break;
         }
+      }
+      return false;
+    },
+    "addUserToGameRoom": function(gameRoomID, username) {
+      let thisRoom = {};
+      let isExist = this.duplicateUser(username, gameRoomID);
+      Rooms.roomList.forEach(function(room) {
+        if (room['gameRoomID'] === gameRoomID) {
+          if (!isExist) {
+            console.log("user added");
+            room.users.push({
+              username: username.toUpperCase(),
+              ready: false
+            });
+            thisRoom = room;
+          } else {
+            thisRoom = null;
+          }
+        }
+      }, thisRoom);
+      synchronize();
+      return thisRoom;
+    },
+    "deleteUserFromGameRoom": function(gameRoomID, username) {
+      let thisRoom = {};
+      Rooms.roomList.forEach(function(room) {
+        if (room['gameRoomID'] === gameRoomID) {
+          room.users = room.users.filter(function(user) {
+            return user.username !== username.toUpperCase()
+          });
+          thisRoom = room;
+        }
+      }, thisRoom);
+      synchronize();
+      return thisRoom;
+    },
+    "startGame": function(gameRoomID) {
+      let thisRoom = {};
+      Rooms.roomList.forEach(function(room) {
+        if (room['gameRoomID'] === gameRoomID) {
+          room.active = true;
+          thisRoom = room;
+        }
+      }, thisRoom);
+      console.log("Backend on its track.. ", thisRoom);
+      synchronize();
+      return thisRoom;
+    },
+    "isGameRoomActive": function(gameRoomID) {
+      for (let i = 0; i < size; i++) {
+        if (Rooms.roomList[i].gameRoomID === gameRoomID) {
+          return roomList[i].active;
+        }
+      }
+    },
+    "setGameRoomActive": function(gameRoomID, boolean) {
+      let success = false;
+      Rooms.roomList.forEach(function(room) {
+        if (room['gameRoomID'] === gameRoomID) {
+          room['active'] = boolean;
+          success = true;
+        }
+      });
+      synchronize();
+      return success;
+    },
+    "setUserReady": function(gameRoomID, username, ready) {
+      let thisRoom = {};
+      for (let i = 0; i < Rooms.roomList.length; i++) {
+        if (Rooms.roomList[i].gameRoomID === gameRoomID) {
+          for (let j = 0; j < Rooms.roomList[i].users.length; j++) {
+            if (Rooms.roomList[i].users[j].username === username.toUpperCase()) {
+              Rooms.roomList[i].users[j].ready = ready;
+              break;
+            }
+          }
+          thisRoom = Rooms.roomList[i];
+          break;
+        }
+      }
+      synchronize();
+      return thisRoom;
+    },
+    "getActiveRooms": function() {
+      let activeGameRooms = [];
+      for (let i = 0; i < Rooms.size; i++)
+        if (Rooms.roomList[i].active === true)
+          activeGameRooms.push(Rooms.roomList[i]);
+      return activeGameRooms;
+    },
+    "getAllRooms": function() {
+      return Rooms.roomList;
+    },
+    "wasPlaying": function(username) {
+      for (let i = 0; i < Rooms.size; i++)
+        for (let j = 0; j < Rooms.roomList[i].users.length; j++)
+          if (Rooms.roomList[i].users[j].username === username.toUpperCase())
+            return true;
+      return false;
     }
+  }
 }();
